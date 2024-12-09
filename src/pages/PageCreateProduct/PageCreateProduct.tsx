01bcd12dc6a86b8cb4d3e715fad217492d4b24a9
maskpa100 <maskpa@yandex.ru>
2024-12-10 02:57:32 +0800

    hh

--- src/pages/PageCreateProduct/PageCreateProduct.tsx
+++
@@ -1,6 +0,0 @@
-import FormProduct from "../../components/FormProduct/FormProduct";
-function PageCreateProduct() {
-  return <FormProduct page="create" />;
-}
-
-export default PageCreateProduct;
--- src/store/slice/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "../store";
-
-export const selectProductById = (state: RootState, productId: number) => {
-  return state.products.products.find((product) => product.id === productId);
-};
